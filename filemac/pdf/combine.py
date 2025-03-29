7c0dbd3412a4f29c1096309055021caa02682299
Your Name <you@example.com>
2025-03-29 11:48:51 +0300

    Update combine.py colors

--- filemac/pdf/combine.py
+++ filemac/pdf/combine.py
@@ -137,9 +137,9 @@ def helpmaster():
         _________________________"""
 
     helper = f"""\n\t---------------------------------------------------------------------------------------------
-        {fcl.BWHITE_FG}|Currently There are 2 supported methods: {FCYAN}Linear and Alternating/shifting.{RESET}\t\t    |
+        {fcl.BWHITE_FG}|Currently There are 2 supported methods: {fcl.FCYAN_FG}Linear and Alternating/shifting.{RESET}\t\t    |
         |-------------------------------------------------------------------------------------------|
-        {fcl.BWHITE_FG}|->Linear pages are ordered in form of: {CYAN}File1Page1,...Fil1Pagen{RESET} then {fcl.CYAN_FG}File2Page1,...Fil2Pagen{RESET}|\n\t{fcl.BWHITE_FG}|File2 is joined at the end of the file1.\t\t\t\t\t\t    |
+        {fcl.BWHITE_FG}|->Linear pages are ordered in form of: {fcl.CYAN_FG}File1Page1,...Fil1Pagen{RESET} then {fcl.CYAN_FG}File2Page1,...Fil2Pagen{RESET}|\n\t{fcl.BWHITE_FG}|File2 is joined at the end of the file1.\t\t\t\t\t\t    |
         |-------------------------------------------------------------------------------------------|
         {fcl.BWHITE_FG}|->Shifting method Picks: {fcl.CYAN_FG}File1Page1, File2Page1...File1pagen,File2Pagen{RESET}\t\t    |
         |--------------------------------------------------------------------------------------------"""
